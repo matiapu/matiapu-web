ce431d594d6aa692b574d100ad039a6216499895
bomuneko <noahkunchan@gmail.com>
2026-06-11 11:55:09 +0900

    feat: ホバー実装

---
+++ src/components/MapArea.jsx
@@ -0,0 +1,11 @@
+import styles from "./MapArea.module.css";
+
+export default function MapArea() {
+  return (
+    <main className={styles.mapArea}>
+      <div className={styles.map}>
+        地図エリア
+      </div>
+    </main>
+  );
+}
