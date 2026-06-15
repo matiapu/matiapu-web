b4f5fb040f7dbd501ecf846db4afaa65c883dfdb
LuckyHolo <keitama2530@gmail.com>
2026-06-16 05:07:35 +0900

    feat: プロフィール、いいねページ作成

---
+++ components/profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import UserIcon from './UserIcon'
+import styles from '@/components/profile.module.css'
+import PostImage from './PostImage'
+
+import { POSTS } from '@/app/(main)/posts/[id]/page' 
+
+function profile() {
+    const post = POSTS[0];
+  return (
+    <div>
+        <UserIcon className={styles.icon}/>
+        <div className={styles.user_info}>
+            <p>{post.address}</p>
+            <h2>{post.name}</h2>
+        </div>
+        <PostImage />
+    </div>
+  )
+}
+
+export default profile
